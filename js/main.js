eb749a67a219174e4296ddd9c69f2727e9a987cb
taurin360 <126446288+taurin360@users.noreply.github.com>
2023-07-07 08:54:25 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 {
+  // -------------------------------------
   // 各カラーボタンの制御
+  // -------------------------------------
   class Color {
     constructor(main, colorNum, sound) {
       this.sound = new Audio(sound);
@@ -25,8 +27,21 @@
       }, 300);
     }
 
+    flashDemo() {
+      this.elemnt.classList.add('flash');
+      setTimeout( () => {
+        this.flashOff();
+      }, 50);
+    }
+
     flashOff() {
       this.elemnt.classList.remove('flash');
+    }
+
+    flashSound() {
+      this.sound.pause();
+      this.sound.currentTime = 0;
+      this.sound.play();
     }
 
     click() {
@@ -36,7 +51,7 @@
       }
       this.main.clickCntUp();
       this.main.check(this.colorNum);
-
+      // フラッシュはCSSの疑似クラスで制御している
       this.sound.pause();
       this.sound.currentTime = 0;
       this.sound.play();
@@ -52,12 +67,17 @@
 
   }
 
+  // -------------------------------------
   // GEME全体の制御
+  // -------------------------------------
   class Main {
-    constructor(maxNum) {
+    constructor(maxNum, start) {
       this.maxNum = maxNum;
+      this.start = start;
       this.cnt = 0;
+      this.demoCnt = 0;
       this.clickCnt = 0;
+      this.arrivalCnt = 0;
       this.colorBtns = [];
       this.questions = [];
 
@@ -68,17 +88,22 @@
         if (this.center.classList.contains('disabled')) {
           return;
         }
-        this.center.classList.add('disabled')
-        this.question();
-        this.centerSound.play();
+        this.center.classList.add('disabled');
+        this.start.setSentenceInactive();
+        this.start.setResultInactive();
+        this.start.setRecInactive();
+        this.demo();
+        setTimeout( () => {
+          this.question();
+        }, 3000);
       });
     }
 
     setup() {
       const sound = ["audio/green.mp3", "audio/red.mp3", "audio/yellow.mp3", "audio/blue.mp3"];
-      this.centerSound = new Audio("audio/center.mp3");
       this.winSound = new Audio("audio/win.mp3");
       this.gameOverSound = new Audio("audio/gameover.mp3");
+      this.congratulation = new Audio("audio/congratulation.mp3");
 
       // カラーボタンのインスタンス作成
       for ( let i = 0; i < 4; i++) {
@@ -98,6 +123,32 @@
       container.appendChild(this.center);
     }
 
+    demo() {
+      let cnt;
+      switch(this.demoCnt) {
+        case 2:
+        case 6:
+        case 10:
+          cnt = 3;
+          break;
+        case 3:
+        case 7:
+        case 11:
+          cnt = 2;
+          break;
+        default:
+          cnt = this.demoCnt % 4;
+      }
+      this.colorBtns[cnt].flashDemo();
+      this.colorBtns[cnt].flashSound();
+      this.demoCnt++;
+      if (this.demoCnt <= 12) {
+        setTimeout( () => {
+          this.demo();
+        }, 100);
+      }
+    }
+
     getId(colorNum) {
       const id = ['green', 'red', 'yellow', 'blue'];
       return id[colorNum];
@@ -106,17 +157,78 @@
     question() {
       this.cnt = 0;
       this.clickCnt = 0;
-      // 0:green 1:red 2:yellow 3:blue
-      const nowBtn = Math.floor(Math.random() * 4);
-      this.questions.push(nowBtn);
+      this.randomGenerate();
       setTimeout( () => {
         this.flash();
       }, 800);
     }
 
+    randomGenerate() {
+      // 0:green 1:red 2:yellow 3:blue
+      let colorNum = [0, 0, 0, 0];
+      this.questions.forEach(question => {
+        if (question === 0) {
+          colorNum[0]++;
+        } else if (question === 1) {
+          colorNum[1]++;
+        } else if (question === 2) {
+          colorNum[2]++;
+        } else if (question === 3) {
+          colorNum[3]++;
+        }
+      });
+
+      // 一度も光ってないボタンは強制的に光らせる
+      if (this.questions.length === 5) {
+        let cnt = undefined;
+        if (colorNum[0] === 0) {
+          cnt = 0;
+        } else if (colorNum[1] === 0) {
+          cnt = 1;
+        } else if (colorNum[2] === 0) {
+          cnt = 2;
+        } else if (colorNum[3] === 0) {
+          cnt = 3;
+        }
+        if (colorNum !== undefined) {
+          this.questions.push(cnt);
+          return;
+        }
+      }
+
+      // 一定回数以上同じボタンが続くのは許容しない
+      let cnt;
+      if (this.questions.length >= 4) {
+        cnt = this.questions.length / 4 + 1;
+        let result = false;
+        let nowBtn;
+        while(result === false) {
+          nowBtn = Math.floor(Math.random() * 4);
+          if (colorNum[0] >= 2 && nowBtn === 0) {
+            continue;
+          } else if (colorNum[1] >= cnt && nowBtn === 1) {
+            continue;
+          } else if (colorNum[2] >= cnt && nowBtn === 2) {
+            continue;
+          } else if (colorNum[3] >= cnt && nowBtn === 3) {
+            continue;
+          } else {
+            result = true;
+          }
+        }
+        this.questions.push(nowBtn);
+        return;
+      }
+
+      // 次のボタンはランダムに選ぶ
+      const nowBtn = Math.floor(Math.random() * 4);
+      this.questions.push(nowBtn);
+    }
+
     flash() {
       const btnNum = this.questions[this.cnt];
       this.colorBtns[btnNum].flash();
+      this.colorBtns[btnNum].flashSound();
       this.cnt++;
       // 出題が全部終わったらボタン押下可能
       if (this.cnt >= this.questions.length) {
@@ -134,12 +246,15 @@
       this.clickCnt++;
     }
 
+    // カラーボタンを押したときに呼ばれ、押し間違いと目標回数をチェックする
     check(colorNum) {
       // 押すボタンを間違えたとき終了
       if (this.questions[this.clickCnt - 1] !== colorNum) {
         this.center.textContent = 'GAME OVER!'
         this.center.classList.add('end');
         this.gameOverSound.play();
+        // 到達回数を設定
+        this.arrivalCnt = this.clickCnt -1;
         setTimeout( () => {
           this.restart();
         }, 3000);
@@ -150,6 +265,8 @@
           this.center.textContent = 'YOU WIN!'
           this.center.classList.add('complete');
           this.winSound.play();
+          // 到達回数を設定
+          this.arrivalCnt = this.clickCnt;
           setTimeout( () => {
             this.restart();
           }, 3000);
@@ -166,25 +283,57 @@
     }
 
     restart() {
+      // 今回の到達回数がこれまでの最高回数を上回ればローカルストレージに保存する
+      const KeyName = 'simoncnt';
+      let num;
+      let recordStr = '';
+
+      this.start.setResultActive();
+      this.start.setRecActive();
+      if (localStorage.getItem(KeyName) === null) {
+        num = 0;
+      } else {
+        num = Number(localStorage.getItem(KeyName));
+      }
+
+      this.start.setResultTextCnt(`到達回数は${this.arrivalCnt}回です。`);
+      if ( num < this.arrivalCnt) {
+        // 最高回数を更新した
+        localStorage.setItem(KeyName, this.arrivalCnt);
+        this.start.setRecTextCnt('最高記録を更新しました！');
+        this.congratulation.play();
+      } else {
+        // 最高回数に届かなかった
+        this.start.setRecTextCnt(`最高記録は${num}回です。`);
+      }
+
       this.center.textContent = 'SIMON'
       this.center.classList.remove('end');
       this.center.classList.remove('complete');
       this.cnt = 0;
       this.clickCnt = 0;
+      this.demoCnt = 0;
       this.questions.length = 0;
       this.center.classList.remove('disabled')
     }
-
   }
 
+  // -------------------------------------
   // 最初の目標回数設定画面
+  // -------------------------------------
   class Start {
     constructor() {
       this.main = document.getElementById('container');
       this.main.classList.add('inactive');
 
       this.sentence = document.getElementById('sentence');
-      this.sentence.classList.add('inactive');
+      this.setSentenceInactive();
+
+      this.result = document.getElementById('result');
+      this.setResultInactive();
+
+      this.rec = document.getElementById('rec');
+      this.setRecInactive();
 
       const start = document.getElementById('start-btn');
       start.addEventListener('click', () => {
@@ -203,8 +352,37 @@
       pre.classList.add('inactive');
       this.main.classList.remove('inactive');
       this.sentence.classList.remove('inactive');
-      new Main(maxNum);
+      new Main(maxNum, this);
     }
+
+    setSentenceInactive() {
+      this.sentence.classList.add('inactive');
+    }
+
+    setResultActive() {
+      this.result.classList.remove('inactive');
+    }
+
+    setRecActive() {
+      this.rec.classList.remove('inactive');
+    }
+
+    setResultInactive() {
+      this.result.classList.add('inactive');
+    }
+
+    setRecInactive() {
+      this.rec.classList.add('inactive');
+    }
+
+    setResultTextCnt(char) {
+      this.result.textContent = char;
+    }
+
+    setRecTextCnt(char) {
+      this.rec.textContent = char;
+    }
+
   }
 
   new Start();
